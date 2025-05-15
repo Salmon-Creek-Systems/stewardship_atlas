a3969814a2f25d31cafb38114e324ab8c3d360ef
root <root@ip-172-31-14-143.us-west-1.compute.internal>
2025-05-15 22:59:06 +0000

    fixes to delta processing

--- python/dataswale_geojson.py
+++ python/dataswale_geojson.py
@@ -1,4 +1,18 @@
 import geojson
+import logging
+
+
+
+# Configure logging
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+from pathlib import Path
+
+import versioning
 
 def create(config) -> str:
     pass
@@ -16,8 +30,10 @@ def refresh_layer(config, name, delta_queue_builder):
     """
     Rebuild the geojson for a layer in the dataswale from the current state of the Delta Queue.
     """
-    layer_path = versioning.atlas_path(config, Path('layers') / name / f'{name}geojson')
+    layer_path = versioning.atlas_path(config, 'layers') / name / f'{name}.geojson'
     fc = delta_queue_builder(config, name)
+    logger.info(f"Writing to {layer_path} FC: {fc}")
+    
     with versioning.atlas_file(layer_path, 'wt') as outfile:
         geojson.dump(fc, outfile)
     return layer_path
