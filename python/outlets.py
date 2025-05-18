9ce2412904927ce330ee0e12aa705b9bdb0c9117
scotfree <gateless@gmail.com>
2025-05-18 20:58:23 +0000

    single webmap tempalte

--- python/outlets.py
+++ python/outlets.py
@@ -1,6 +1,22 @@
-def map_json(config, name):
+import subprocess
+import json
+
+
+import utils
+import versioning
+import logging
+
+
+# Configure logging
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+def webmap_json(config, name):
     # Calculate center and zoom from bbox
-    bbox = swale_config['geometry']['bbox']
+    bbox = config['dataswale']['bbox']
     center_lat = (bbox['north'] + bbox['south']) / 2
     center_lon = (bbox['east'] + bbox['west']) / 2 
     lat_diff = bbox['north'] - bbox['south']
@@ -18,35 +34,35 @@ def map_json(config, name):
     map_sources = {}
     map_layers = []
     outlet_config = config['assets'][name]
-
+    layers_dict = {x['name']: x for x in config['dataswale']['layers']}
+    
     # for each layer, we add a source and display layer, and possibly a label layer
     for layer_name in outlet_config['in_layers']:
-        layer = config['layers'][layer_name]
-        sources[layer_name] =  {
-                'type': 'image' if layer['type'] == 'raster' else 'geojson',
-                'url': f'../layers/{layer_name}/{layer_name}.{layer['type']}',
-                'coordinates': bbox_to_corners(config['geometry']['bbox'])
+        layer = layers_dict[layer_name]
+        map_sources[layer_name] =  {
+                'type': 'image' if layer['datatype'] == 'raster' else 'geojson',
+                'url': f"../layers/{layer_name}/{layer_name}.{layer['datatype']}",
+                'coordinates': utils.bbox_to_corners(config['dataswale']['bbox'])
             }
         map_layer = {
                 'id': f"{layer_name}-layer",
-                'type': layer['type'],
+                'type': layer['datatype'],
                 'source': layer_name
                 }
-        if layer['type'] == 'raster':
+        if layer['datatype'] == 'raster':
             map_layer['paint'] = {
                     "raster-opacity": 0.1,
                     "raster-contrast": 0.3}
-        elif layer['type'] == 'vector':
+        elif layer['datatype'] == 'vector':
             map_layer['paint'] = {
-                    "fill-color": rgb_to_css(a.get('fill_color', [150,150,150])),
-                    "color": rgb_to_css(a.get('color', [150,150,150])),
-                    "line-color": rgb_to_css(a.get('color', [150,150,150])) ,
+                    "fill-color": utils.rgb_to_css(layer.get('fill_color', [150,150,150])),
+                    "color": utils.rgb_to_css(layer.get('color', [150,150,150])),
+                    "line-color": utils.rgb_to_css(layer.get('color', [150,150,150])) ,
                      'line-width': ['get', 'vector_width']
             }
 
         map_layers.append(map_layer)
-        if False: #layer.get('add_labels', False):
-            
+        if False: #layer.get('add_labels', False):            
             label_layer = {
                     "id": f"{label_name}-label-layer",
                     "type": "symbol",
@@ -57,7 +73,8 @@ def map_json(config, name):
                         "text-font": ["Open Sans Regular"],
                         "text-field": ["get", "name"],
                         "text-size": 20
-                        }})
+                        }
+                }
  
             if  layer.get('feature_type', 'line') == 'note':    
                 label_layer.update({
@@ -67,7 +84,7 @@ def map_json(config, name):
                         'text-halo-color': '#FF9999',
                         'text-color': '#000000'
                     }
-                    })
+                })
             map_layers.append(label_layer)
         #else:
         #    logger.error(f"not an outlet layer: {layer_name}.")
@@ -76,17 +93,19 @@ def map_json(config, name):
   
     return map_config
 
-def generate_map_page(title, map_config, output_path):
+def generate_map_page(title, map_config_data, output_path):
     """Generate the complete HTML page for viewing a map"""
     # Read template files
     with open('../templates/map.html', 'r') as f:
         template = f.read()
-
-    with open(output_path, 'w') as f:
-        f.write(template.format(
+    logger.info(f"About to generate HTML to {output_path}: {template}.")
+    processed_template = template.format(
             title=title,
-            map_config=json.dump(map_config,  indent=2)
-        ))
+            map_config=json.dumps(map_config_data,  indent=2))
+
+    with open(output_path, 'w') as f_out:
+      f_out.write(processed_template)          
+        
 
 
 
@@ -104,6 +123,7 @@ def outlet_webmap(config, name):
     
 
     # Generate base map configuration
+    
     map_config = webmap_json(config, name)
     webmap_dir = versioning.atlas_path(config, "outlets") / name
     webmap_dir.mkdir(parents=True, exist_ok=True)
@@ -112,12 +132,19 @@ def outlet_webmap(config, name):
     # make a JPG of basemap tiff..
     # TODO this should be a tiling URL instead of a local file..
     # TODO maybe resampling happens here?
-    tiff2jpg(f"{basemap_dir}/basemap.tiff", webmap_dir / "basemap.jpg")
+    # TODO here is where we should be using Dagster and actual asset mgmt
+    basemap_path = basemap_dir / "basemap.jpg"
+    if True: #basemap_path.exists():
+        logger.info(f"Using extant basemap: {basemap_path}.")
+    else:
+        logger.info(f"Generating basemap: {basemap_path}.")
+        utils.tiff2jpg(f"{basemap_dir}/basemap.tiff", basemap_path)
     
     subprocess.run(['cp', '-r', '../templates/css/', webmap_dir / "css"])
     #subprocess.run(['cp', '../templates/js/map.js', f"{webmap_dir}/js/"])
     
     output_path = webmap_dir / "index.html"
+    logger.info(f"Creating webmap HTML in {output_path}.")
     html_path = generate_map_page("test Webmap", map_config, output_path)  
   
     return output_path
