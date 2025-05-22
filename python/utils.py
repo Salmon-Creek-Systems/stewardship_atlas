205e0194c6c65df3fba2f8be1b647d03634d948a
scotfree <gateless@gmail.com>
2025-05-22 20:09:31 +0000

    utils

---
+++ python/utils.py
@@ -0,0 +1,71 @@
+
+import logging, subprocess
+
+# Configure logging
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+def rgb_to_css(rgb_tuple):
+    return f'rgb({rgb_tuple[0]}, {rgb_tuple[1]}, {rgb_tuple[2]})'
+             
+#def line_width(width_base, width_delta):
+#    return width_base + width_delta
+
+def bbox_to_corners(b):
+    return [
+        [b['west'], b['north']],
+        [b['east'], b['north']],
+        [b['east'], b['south']],
+        [b['west'], b['south']],
+        ]
+
+def bbox_to_polygon(b):
+    corners = bbox_to_corners(b)
+    return corners + [corners[0]]
+
+
+def tiff2jpg(tiff_path, atlas_config=None, swale_config=None):
+    """Convert TIFF to JPG using versioned paths"""
+    # Construct JPG path
+    jpg_path = tiff_path + ".jpg"
+    
+    
+    logger.debug(f"Converting TIFF to JPG: {jpg_path}")
+    subprocess.check_output(['gdal_translate', '-b', '1', '-scale',tiff_path, jpg_path])
+    
+    return jpg_path
+
+def resample_raster_gdal(config, inpath, resample_width=400):
+    """Resample raster to target CRS using versioned paths"""
+    logger.info(f"Resampling for: [{inpath}]")
+    inpath = Path(inpath)
+    temp_path = inpath.parent / f"tmp.{inpath.name}"
+
+    # Move input to temp path
+    os.rename(inpath, temp_path)
+    logger.debug(f"Resampling raster to {config['dataswale']['crs']} @ width {resample_width}: {inpath}")
+    
+    # Perform resampling
+    subprocess.check_output([
+        'gdalwarp', '-r', 'bilinear',
+        '-ts', str(resample_width), '0',
+        '-t_srs', config['dataswale']['crs'],
+        str(temp_path), str(inpath)
+    ])
+    # TODO - remove temp path
+    return inpath
+
+
+
+def set_crs_raster(config, inpath):
+    """Set CRS for raster using versioned paths"""
+    inpath = Path(inpath)
+    temp_path = inpath.parent / f"tmp.{inpath.name}"
+    logger.debug(f"Setting raster CRS to {config['dataswale']['crs']}: {inpath}")
+    subprocess.check_output(['gdalwarp', '-t_srs', config['dataswale']['crs'], str(inpath), str(temp_path)])
+    # Move temp file to final location
+    os.rename(temp_path, inpath)  
+    return inpath
